ec38fb8516dad1bd373d62a1ebf9999c3d3b8215
Abgar <abgar1223@gmail.com>
2019-03-31 19:41:01 +0300

    Renamed folder

---
+++ SearchAndSort/DataService.cpp
@@ -0,0 +1,73 @@
+#include <utility>
+
+//
+// Created by user on 09.03.2019.
+//
+
+
+#include <iostream>
+#include "DataService.h"
+
+
+DataService::DataService() {
+
+    this->fileName = R"(D:\University\ASDC\SearchAndSort\dataFile.txt)";
+    this->inFile.open(fileName);
+
+    this->sorted = false;
+
+    //Check that the file is available and was opened
+    if (!inFile) {
+        std::cerr << "Unable to open file datafile.txt";
+        exit(1);   // call system to stop
+    }
+}
+
+DataService::~DataService() {
+    //Close the stream
+    if (inFile) {
+        inFile.close();
+    }
+}
+
+void DataService::readDataFromFile() {
+
+
+    int key;
+    std::string name;
+    std::string surname;
+    int age;
+    char gender;
+    float grade;
+
+
+    while (inFile >> key >> name >> surname >> age >> gender >> grade) {
+        people.push_back(DataModel(key, name, surname, age, gender, grade));
+    }
+
+    for (int i = 0; i < people.size(); i++) {
+        this->dataArray[i] = &people[i];
+    }
+
+    this->recordCount = people.size();
+
+
+    this->printData();
+}
+
+void DataService::printData() {
+
+    std::cout << "Print Table data \n";
+    for (int i = 0; i < recordCount; i++) {
+        std::cout <<
+                  this->dataArray[i]->key << ".\t" <<
+                  this->dataArray[i]->name << "\t" <<
+                  this->dataArray[i]->surname << "\t" <<
+                  this->dataArray[i]->age << "\t" <<
+                  this->dataArray[i]->gender << "\t" <<
+                  this->dataArray[i]->grade << "\t" << std::endl;
+    }
+}
+
+
+
---
+++ SearchAndSort/cmake-build-debug/CMakeFiles/TableAccess.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/University/ASDC/SearchAndSort/BubbleSort.cpp" "D:/University/ASDC/SearchAndSort/cmake-build-debug/CMakeFiles/TableAccess.dir/BubbleSort.cpp.obj"
+  "D:/University/ASDC/SearchAndSort/DataModel.cpp" "D:/University/ASDC/SearchAndSort/cmake-build-debug/CMakeFiles/TableAccess.dir/DataModel.cpp.obj"
+  "D:/University/ASDC/SearchAndSort/DataService.cpp" "D:/University/ASDC/SearchAndSort/cmake-build-debug/CMakeFiles/TableAccess.dir/DataService.cpp.obj"
+  "D:/University/ASDC/SearchAndSort/main.cpp" "D:/University/ASDC/SearchAndSort/cmake-build-debug/CMakeFiles/TableAccess.dir/main.cpp.obj"
+  "D:/University/ASDC/SearchAndSort/searchingAlgorithms/BinarySearch.cpp" "D:/University/ASDC/SearchAndSort/cmake-build-debug/CMakeFiles/TableAccess.dir/searchingAlgorithms/BinarySearch.cpp.obj"
+  "D:/University/ASDC/SearchAndSort/searchingAlgorithms/HashTableSearch.cpp" "D:/University/ASDC/SearchAndSort/cmake-build-debug/CMakeFiles/TableAccess.dir/searchingAlgorithms/HashTableSearch.cpp.obj"
+  "D:/University/ASDC/SearchAndSort/searchingAlgorithms/SequentialSearch.cpp" "D:/University/ASDC/SearchAndSort/cmake-build-debug/CMakeFiles/TableAccess.dir/searchingAlgorithms/SequentialSearch.cpp.obj"
+  "D:/University/ASDC/SearchAndSort/searchingAlgorithms/UnsortedTreeTableSearch.cpp" "D:/University/ASDC/SearchAndSort/cmake-build-debug/CMakeFiles/TableAccess.dir/searchingAlgorithms/UnsortedTreeTableSearch.cpp.obj"
+  "D:/University/ASDC/SearchAndSort/sortingAlgorithms/HeapSort.cpp" "D:/University/ASDC/SearchAndSort/cmake-build-debug/CMakeFiles/TableAccess.dir/sortingAlgorithms/HeapSort.cpp.obj"
+  "D:/University/ASDC/SearchAndSort/sortingAlgorithms/InsertionSort.cpp" "D:/University/ASDC/SearchAndSort/cmake-build-debug/CMakeFiles/TableAccess.dir/sortingAlgorithms/InsertionSort.cpp.obj"
+  "D:/University/ASDC/SearchAndSort/sortingAlgorithms/QuickSort.cpp" "D:/University/ASDC/SearchAndSort/cmake-build-debug/CMakeFiles/TableAccess.dir/sortingAlgorithms/QuickSort.cpp.obj"
+  "D:/University/ASDC/SearchAndSort/sortingAlgorithms/SelectionSort.cpp" "D:/University/ASDC/SearchAndSort/cmake-build-debug/CMakeFiles/TableAccess.dir/sortingAlgorithms/SelectionSort.cpp.obj"
+  "D:/University/ASDC/SearchAndSort/sortingAlgorithms/ShellSort.cpp" "D:/University/ASDC/SearchAndSort/cmake-build-debug/CMakeFiles/TableAccess.dir/sortingAlgorithms/ShellSort.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../searchingAlgorithms"
+  "../,"D:/University/ASDC/SearchAndSort/sortingAlgorithms""
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
